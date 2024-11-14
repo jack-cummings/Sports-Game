70f04d291345039f70a345f511b194a9369ca64e
jack-cummings <jmcummi2@ncsu.edu>
2024-11-14 16:32:35 -0500

    added two games

--- sports-app/src/game_list.js
+++ sports-app/src/game_list.js
@@ -1,5 +1,5 @@
 export const game_list = [
-    ["11/14/2024", "Tennessee Titans", "Washington Commanders", "Philadelphia, PA 8:20 EST"],
-    ["11/15/2024", "Miami Heat", "Indiana Pacers", 'Indianapolis, IN 7:00 EST' ]
-
+    ["11/14/2024", "Washington Commanders", "Philidelphia Eagles", "Philadelphia, PA 8:20 EST"],
+    ["11/15/2024", "Miami Heat", "Indiana Pacers", 'Indianapolis, IN 7:00 EST' ],
+    ["11/16/2024", "St. Lous Blues", "Boston Bruins", 'Boston, MA 1:00 EST' ],
 ]
