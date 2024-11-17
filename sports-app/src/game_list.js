4947b1cb07da739bdda1c0e0b2f101da1abf22c0
jack-cummings <jmcummi2@ncsu.edu>
2024-11-17 11:31:42 -0500

    add week of games

--- sports-app/src/game_list.js
+++ sports-app/src/game_list.js
@@ -1,5 +1,14 @@
 export const game_list = [
     ["11/14/2024", "Washington Commanders", "Philadelphia Eagles", "Philadelphia, PA 8:20 EST"],
     ["11/15/2024", "Miami Heat", "Indiana Pacers", 'Indianapolis, IN 7:00 EST' ],
-    ["11/16/2024", "St. Lous Blues", "Boston Bruins", 'Boston, MA 1:00 EST' ]
+    ["11/16/2024", "St. Lous Blues", "Boston Bruins", 'Boston, MA 1:00 EST' ],
+    ["11/17/2024", "Kansas City Chiefs", "Buffalo Bills", "Buffalo, NY"],
+    ["11/18/2024", "Philadelphia 76ers", "Miami Heat", "Miami, FL"],
+    ["11/19/2024", "Denver Nuggets", "Memphis Grizzlies", "Memphis, TN"],
+    ["11/20/2024", "Carolina Hurricanes", "Philadelphia Flyers", "Philadelphia, PA"],
+    ["11/21/2024", "Minnesota Timberwolves", "Toronto Raptors", "Toronto, ON"],
+    ["11/22/2024", "Boston Celtics", "Washington Wizards", "Washington, DC"],
+    ["11/23/2024", "Vegas Golden Knights", "Montreal Canadiens", "Montreal, QC"],
+    ["11/24/2024", "Minnesota Vikings", "Chicago Bears", "Chicago, IL"],
+    
 ]
